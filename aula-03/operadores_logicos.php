e6e8d79e66bdb8fa791055c0a8ccaa4301a9ca3f
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-06-01 19:10:17 -0300

    feat: operators

---
+++ aula-03/operadores_logicos.php
@@ -0,0 +1,22 @@
+<?php
+
+$numero = 10;
+
+/*
+
+== (igualdade)
+=== (idencidade)
+> (maior que)
+< (menor que)
+>= (maior ou igual)
+<= (menor ou igual)
+
+! (negação)
+!= (negar a igualdade)
+!== (negar a idencidade)
+
+*/
+
+echo $numero === 10; // 1 = true
+echo "<br>";  
+echo $numero === 11; // nao mostra nado pois o resultado e 0 = false
