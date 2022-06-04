6637156dd135dda7766aa880ae9ec2f0d6fe18e1
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-06-04 16:16:35 -0300

    feat: function folder

---
+++ aula-11/diretorio.php
@@ -0,0 +1,12 @@
+<?php
+
+mkdir('test'); // criara a pasta test
+mkdir('test/fotos'); // criara a pasta fotos dentro test
+mkdir('test/fotos/2022'); // criara a pasta 2022 dentro de fotos
+mkdir('test/fotos/2022/ferias'); // criara a pasta ferias dentro de 2022
+
+// remove um diretorio (apaga)
+rmdir();
+
+// muda as permições do diretorio
+chdir();
